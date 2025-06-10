9f0614b26d60c5d7dd37629d6ee75a4249e6530e
danil-andreevich <danil_shentyakov@mail.ru>
2025-06-10 19:38:07 +0500

    первая версия модели бас

--- bas_project/dictionary/models.py
+++ bas_project/dictionary/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class DroneTerm(models.Model):
-    term = models.CharField("Термин", max_length=100)
-    translation = models.CharField("Перевод", max_length=100)
-    definition = models.TextField("Определение") 
+    term_eng = models.CharField("Термин", max_length=100)
+    abbr_eng = models.CharField("Термин на английском", max_length=100)
+    category = models.CharField("Категория", max_length=100)
+    part_references = models.CharField("Часть речи", max_length=100)
+    term_rus = models.CharField("Термин на русском",max_length=100)
+    definition_rus = models.TextField("Определение на русском")
+    definition_eng = models.TextField("Определение на английском")
+    context_eng = models.TextField("Контекст на английском")
+    context_rus = models.TextField("Контекст на русском")
 
     def __str__(self):
-        return self.term
+        return self.term_eng
