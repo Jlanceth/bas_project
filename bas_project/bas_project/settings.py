8c6393e478be775499b81f0ed7438271135769e7
Данил Сахаутдинов <danilgul90@gmail.com>
2025-06-15 01:12:44 +0500

    upgrade legal, supply, dpas

--- bas_project/bas_project/settings.py
+++ bas_project/bas_project/settings.py
@@ -134,11 +134,4 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
